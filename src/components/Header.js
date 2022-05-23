83e84d00bf079eddfa5cbdf785f3d375550e00a8
Marcus Yeo <66177132+MYB0SS@users.noreply.github.com>
2022-05-23 22:26:45 +0800

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// rafce
+const Header = () => {
+  return (
+    <>
+      <h1>Welcome to Hangman for Tiktok</h1>
+      <p>Guess the hidden word by entering a letter, good luck!</p>
+    </>
+  )
+}
+
+export default Header
