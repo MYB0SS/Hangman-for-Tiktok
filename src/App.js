7fce7317699bcc0d8207c5aaec19213f244fb08d
Marcus Yeo <mjtan10@gmail.com>
2022-05-24 23:03:43 +0800

    Edited twitter name

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { showNotification as show, checkWin} from './helpers/helpers';
 
 import './App.css';
 
-const words = ['tiktok', 'youtube', 'instagram', 'reddit', 'facebook', 'snapchat', 'twiiter'];
+const words = ['tiktok', 'youtube', 'instagram', 'reddit', 'facebook', 'snapchat', 'twitter'];
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
 function App() {
